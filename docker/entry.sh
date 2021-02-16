2b197e3f3e2413e1f0c929e190b5ee512c3d7162
Roland Kammerer <roland.kammerer@linbit.com>
2021-02-16 12:50:09 +0100

    packaging: add systemd service file
    While at it fix some minor things packaging was complaining (like the
    extended description).

--- docker/entry.sh
+++ docker/entry.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-TMDIR="$(mktemp -d)"
 
 cd /
-cd $TMPDIR && cp -r /src/* .
+mkdir -p /tmp/src
+cd /tmp/src && cp -r /src/* .
 
 source "$HOME/.cargo/env"
 case $1 in
 	rpm)
 		USER=builder cargo rpm init
-		sed -i 's/^Release:.*/Release: @@RELEASE@@/' .rpm/drbdd.spec
+		(cd .rpm && patch < ../docker/drbdd.spec.patch)
 		cargo rpm build
 		find ./target/release/rpmbuild/RPMS/ -name "*.rpm" -exec cp {} /out \;
 		;;
