c04eb62b4c86ea23a4d12fe1a99e5d6483acffd2
Roland Kammerer <roland.kammerer@linbit.com>
2021-02-18 15:23:08 +0100

    build,rpm: fix docker build

--- docker/entry.sh
+++ docker/entry.sh
@@ -10,6 +10,7 @@ source "$HOME/.cargo/env"
 case $1 in
 	rpm)
 		VERSION="$(awk -F '=' '/^version/ {gsub(/"/, "", $2); gsub(/ /, "", $2); print $2}' Cargo.toml)"
+		install /dev/null /usr/bin/lbvers.py
 		make debrelease VERSION="$VERSION"
 		mkdir -p "$(rpm -E "%_topdir")/SOURCES"
 		mv "./drbdd-${VERSION}.tar.gz" "$(rpm -E "%_topdir")/SOURCES"
