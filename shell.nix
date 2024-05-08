07f92d9cc7ef429a19ddd414205db6fda60ce871
Roland Kammerer <roland.kammerer@linbit.com>
2024-05-08 09:48:19 +0200

    build: add shell.nix
    I had that around for some time. The last missing piece was
    debian/changelog generation. I used gbp, which is not in nixpkgs and I
    don't care packaging it. Now I just use "mini-gbp-dch" and that is easy
    enough with some shell and awk (build-helpers:mini-gbp-dch.sh).

---
+++ shell.nix
@@ -0,0 +1,10 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell {
+    # build time deps
+    nativeBuildInputs = with pkgs; [ rustc cargo ];
+    # other useful pkgs
+    # for debian/changelog generation use internal build-helpers:mini-gbp-dch.sh
+    # git for release commit, make for make debrelease, debian-devscripts for dch
+    # python + toml for build-helpers:lbvers.py
+    packages = with pkgs; [ git make python3 python3Packages.toml debian-devscripts ];
+}
